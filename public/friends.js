33b6587dc18a9041699a98e74750248c7bf2fe1a
Angel Martinez <martinezangel98@hotmail.com>
2020-01-02 21:30:52 -0500

    my first commit

---
+++ public/friends.js
@@ -0,0 +1,32 @@
+var friends = [ {
+    "name" : "Jose",
+    "photo" : "http://www.mens-hairstylists.com/wp-content/uploads/2015/10/stylish-haircuts-for-men.jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Shannon",
+    "photo" : "http://science-all.com/images/wallpapers/girl-pictures/girl-pictures-23.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Karen",
+    "photo" : "http://images.zap2it.com/assets/p9257057_n160758_cc_v4_aa/guys-with-kids.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Madison",
+    "photo" : "http://www.tenerewilliams.com/wp-content/uploads/2013/02/Naomi-Grossman-headshot.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Sean",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/236x/e6/58/db/e658dbeb3518edeab6a19516b8146137.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Shana",
+    "photo" : "http://cdn3.thr.com/sites/default/files/2013/02/emma_roberts.jpg",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Pavel",
+    "photo" : "https://s-media-cache-ak0.pinimg.com/564x/02/72/d2/0272d2536de31958c6dcb3bb1b447586.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, 
+];
+
+module.exports = friends;
